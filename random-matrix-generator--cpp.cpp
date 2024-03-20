7e704c48fe7cf3bb58d0757505a172ff9530cb20
Yusuf Can Yıldırım <73697361+Joddey@users.noreply.github.com>
2024-03-20 17:26:07 +0300

    Update random-matrix-generator--cpp.cpp

--- random-matrix-generator--cpp.cpp
+++ random-matrix-generator--cpp.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main() {
     int rows, cols;
 
-    cout << "Enter the number of rows: ";
+    cout << "Enter the number of rows.: ";
     cin >> rows;
     cout << "Enter the number of columns: ";
     cin >> cols;
