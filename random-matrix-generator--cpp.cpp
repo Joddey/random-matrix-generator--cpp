538c36d9ea17478a9fde4040bd120d3ee0c45a78
Yusuf Can Yıldırım <73697361+Joddey@users.noreply.github.com>
2024-03-20 03:09:09 +0300

    Add files via upload
    This C++ program generates a random matrix with dimensions provided by the user.

---
+++ random-matrix-generator--cpp.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    int rows, cols;
+
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+    cout << "Enter the number of columns: ";
+    cin >> cols;
+
+    srand(time(0));
+
+    cout << "Random Matrix:\n";
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout << rand() % 10 << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
